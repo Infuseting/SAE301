e0605df8f9d00187d2c8f2199d5f28e7fccb6069
Infuseting <tuture27100@gmail.com>
2026-01-07 10:34:55 +0100

    fix: add role and permission directly in migrate

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -13,41 +13,9 @@ class RolesAndPermissionsSeeder extends Seeder
      */
     public function run(): void
     {
+        // Logic moved to migration: 2026_01_07_093259_create_roles_and_permissions.php
+        
         // Reset cached roles and permissions
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-
-        // create permissions
-        $permissions = [
-            'view users',
-            'edit users',
-            'delete users',
-            'view logs',
-            'grant role',
-            'grant admin',
-            'access-admin',
-            // Club permissions
-            'accept-club',
-            'manage-own-club',
-            'view-clubs',
-            'create-club',
-        ];
-
-        foreach ($permissions as $permission) {
-            Permission::firstOrCreate(['name' => $permission]);
-        }
-
-        // create roles and assign created permissions
-
-        // User Role
-        $userRole = Role::firstOrCreate(['name' => 'user']);
-        $userRole->givePermissionTo(['view-clubs', 'create-club']);
-
-        // Club Manager Role
-        $clubManagerRole = Role::firstOrCreate(['name' => 'club-manager']);
-        $clubManagerRole->givePermissionTo(['manage-own-club', 'view-clubs']);
-
-        // Admin Role
-        $adminRole = Role::firstOrCreate(['name' => 'admin']);
-        $adminRole->givePermissionTo(Permission::all());
     }
 }
